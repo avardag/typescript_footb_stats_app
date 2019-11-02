38b3ff5a689e1aa0178c7078730f95c9efa34847
Dagestan05 <steklofan@gmail.com>
2019-11-02 01:23:48 +0100

    addedd htmlReport class

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { MatchReader } from './MatchReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HTMLReport } from './reportTargets/HTMLReport';
 
 //Create an obj that satisfies the 'DataReader' interface
 const csvFileReader = new CSVFileReader('football.csv');
@@ -15,8 +16,8 @@ matchReader.load()
 
 //Build Summary
 const summary = new Summary(
-  new WinsAnalysis("Chelsea"),
-  new ConsoleReport()
+  new WinsAnalysis("Everton"),
+  new HTMLReport('EvertonReport')
 )
 
 summary.buildAndPrintReport(matchReader.matches)
